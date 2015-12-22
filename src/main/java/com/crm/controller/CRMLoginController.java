2febe165b5395c0cb13a7958d92de7cf084af33f
Kapil.sharma <kapilsharma@Administrators-MacBook-Pro-57.local>
2015-12-22 18:53:00 +0530

    Earnify CRM Commit
    Earnify CRM Commit

---
+++ src/main/java/com/crm/controller/CRMLoginController.java
@@ -0,0 +1,22 @@
+package com.crm.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class CRMLoginController {
+	private static final Logger log = LoggerFactory.getLogger(CRMLoginController.class);
+	
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public ModelAndView getLoginDefaultPage(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView model = new ModelAndView("login");
+		return model;
+	}
+}
