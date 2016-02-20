7fc5268fd2408953aab43b6220e5fa9caadfc6bd
Sandeep Pandey <sandeep.pandey@snapdeal.com>
2016-02-20 23:40:06 +0530

    Commiting Campaign Flow

---
+++ src/main/java/com/crm/earnify/base/json/JSONizable.java
@@ -0,0 +1,13 @@
+/*  Copyright 2016 Jasper Infotech (P) Limited . All Rights Reserved.
+ *  JASPER INFOTECH PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+*/  
+package com.crm.earnify.base.json;
+
+/**
+ *  @version     1.0, 19/2/16
+ *  @author sandeepandey
+ */
+
+public interface JSONizable {
+    public String toJSONString();
+}
